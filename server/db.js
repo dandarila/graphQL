26e01d0de5e6940df970720d7575be19e2cd9492
mdandarila-wb <marian.dandarila@whitbread.com>
2023-02-06 18:03:28 +0200

    mac to git

--- server/db.js
+++ server/db.js
@@ -5,3 +5,4 @@ const db = new Database('./data');
 export const Company = db.table('companies');
 export const Job = db.table('jobs');
 export const User = db.table('users');
+
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,16 +1,8 @@
+
+import { Job } from './db.js';
+
 export const resolvers = {
 	Query: {
-		jobs: () => [
-			{
-				id: "id1",
-				title: "Title1",
-				description: "Some desc 11",
-			},
-			{
-				id: "id2",
-				title: "Title2",
-				description: "Some desc 22",
-			},
-		],
+		jobs: () => Job.findAll(),
 	},
 };
